6b1a22db7aca6592b310db86e7c789fd8ba6cc3f
Bui Xuan huynh <huynhxuanbui@gmail.com>
2020-05-17 21:09:52 +0700

    create class Category

---
+++ src/model/Category.java
@@ -0,0 +1,34 @@
+package model;
+
+public class Category {
+    private String categoryId;
+    private String categoryName;
+
+    public Category() {
+    }
+
+    public Category(String categoryName) {
+        this.categoryName = categoryName;
+    }
+
+    public Category(String categoryId, String categoryName) {
+        this.categoryId = categoryId;
+        this.categoryName = categoryName;
+    }
+
+    public String getCategoryId() {
+        return categoryId;
+    }
+
+    public void setCategoryId(String categoryId) {
+        this.categoryId = categoryId;
+    }
+
+    public String getCategoryName() {
+        return categoryName;
+    }
+
+    public void setCategoryName(String categoryName) {
+        this.categoryName = categoryName;
+    }
+}
