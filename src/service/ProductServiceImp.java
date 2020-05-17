6cfffe81623df13419042df549db4c2eb0940a6e
Bui Xuan huynh <huynhxuanbui@gmail.com>
2020-05-17 21:31:46 +0700

    get list product

--- src/service/ProductServiceImp.java
+++ src/service/ProductServiceImp.java
@@ -1,28 +1,86 @@
 package service;
 
+import model.DBConnect;
 import model.Product;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
 public class ProductServiceImp implements ProductService{
-    static List<Product> productList;
-    static {
-        productList = new ArrayList<>();
-        productList.add(new Product("PR001","IPHONE XS",8500000,21,"/img/phone1.png",1,"OK","SmartPhone"));
-        productList.add(new Product("PR001","IPHONE XS",8500000,21,"/img/phone1.png",1,"OK","SmartPhone"));
-        productList.add(new Product("PR001","IPHONE XS",8500000,21,"/img/phone1.png",1,"OK","SmartPhone"));
-        productList.add(new Product("PR001","IPHONE XS",8500000,21,"/img/phone1.png",1,"OK","SmartPhone"));
-        productList.add(new Product("PR001","IPHONE XS",8500000,21,"/img/phone1.png",1,"OK","SmartPhone"));
-        productList.add(new Product("PR001","IPHONE XS",8500000,21,"/img/phone1.png",1,"OK","SmartPhone"));
-    }
+
     @Override
-    public List<Product> showAllProduct() {
-        return productList;
+    public List<Product> getListProduct() throws SQLException {
+        Connection connection = DBConnect.getConnection();
+        String sql = "select * from product";
+        PreparedStatement ps = connection.prepareCall(sql);
+        ResultSet rs = ps.executeQuery();
+        ArrayList<Product> products = new ArrayList<>();
+        while (rs.next()) {
+            Product product = new Product();
+            product.setProductId(rs.getString("ProductID"));
+            product.setCategoryId(rs.getString("CategoryId"));
+            product.setProductName(rs.getString("ProductName"));
+            product.setProductPrice(rs.getFloat("ProductPrice"));
+            product.setQuantityInStock(rs.getInt("QuantityInStock"));
+            product.setImage(rs.getString("Image"));
+            product.setStatus(rs.getInt("Status"));
+            product.setDescription(rs.getString("Description"));
+            products.add(product);
+        }
+        return products;
     }
 
     @Override
-    public void saveProduct(Product product) {
-        productList.add(product);
+    public List<Product> getListProduct(String categoryId) throws SQLException {
+        return null;
+    }
+
+    @Override
+    public List<Product> getListProductIP() throws SQLException {
+        Connection connection = DBConnect.getConnection();
+        String sql = "select product.* from product where ProductID like '%IP%' limit 3";
+        PreparedStatement ps = connection.prepareCall(sql);
+        ResultSet rs = ps.executeQuery();
+        ArrayList<Product> products = new ArrayList<>();
+        while (rs.next()) {
+            Product product = new Product();
+            product.setProductId(rs.getString("ProductID"));
+            product.setCategoryId(rs.getString("CategoryId"));
+            product.setProductName(rs.getString("ProductName"));
+            product.setProductPrice(rs.getFloat("ProductPrice"));
+            product.setQuantityInStock(rs.getInt("QuantityInStock"));
+            product.setImage(rs.getString("Image"));
+            product.setStatus(rs.getInt("Status"));
+            product.setDescription(rs.getString("Description"));
+            products.add(product);
+        }
+        return products;
+    }
+
+    @Override
+    public ArrayList<Product> getListProductSS() throws SQLException {
+        Connection connection = DBConnect.getConnection();
+        String sql = "\n" +
+                "select product.* from product where ProductID like '%SS%' limit 3";
+        PreparedStatement ps = connection.prepareCall(sql);
+        ResultSet rs = ps.executeQuery();
+        ArrayList<Product> products = new ArrayList<>();
+        while (rs.next()) {
+            Product product = new Product();
+            product.setProductId(rs.getString("ProductID"));
+            product.setCategoryId(rs.getString("CategoryId"));
+            product.setProductName(rs.getString("ProductName"));
+            product.setProductPrice(rs.getFloat("ProductPrice"));
+            product.setQuantityInStock(rs.getInt("QuantityInStock"));
+            product.setImage(rs.getString("Image"));
+            product.setStatus(rs.getInt("Status"));
+            product.setDescription(rs.getString("Description"));
+            products.add(product);
+        }
+        return products;
     }
 }
