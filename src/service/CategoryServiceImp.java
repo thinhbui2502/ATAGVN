74cd291d2e6e62f74bedcfa1677878cd9833485d
Bui Xuan huynh <huynhxuanbui@gmail.com>
2020-05-17 21:14:49 +0700

    get list category

---
+++ src/service/CategoryServiceImp.java
@@ -0,0 +1,41 @@
+package service;
+
+import model.Category;
+import model.DBConnect;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class CategoryServiceImp implements CategoryService {
+    @Override
+    public List<Category> getListCategory() throws SQLException {
+        Connection connection = DBConnect.getConnection();
+        String sql = "SELECT * FROM category";
+        PreparedStatement ps = connection.prepareCall(sql);
+        ResultSet rs = ps.executeQuery();
+        ArrayList<Category> list = new ArrayList<>();
+        while (rs.next()) {
+            Category category = new Category();
+            category.setCategoryId(rs.getString("CategoryID"));
+            category.setCategoryName(rs.getString("CategoryName"));
+            list.add(category);
+        }
+        return list;
+    }
+
+    public static void main(String[] args) {
+        CategoryServiceImp categoryServiceImp = new CategoryServiceImp();
+        try {
+            for (Category c : categoryServiceImp.getListCategory()
+            ) {
+                System.out.println(c.getCategoryName());
+            }
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+    }
+}
