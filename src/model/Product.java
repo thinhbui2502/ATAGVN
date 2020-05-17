b9882b30241b0931c0370d1b50b946c3f4d43fc6
Bui Xuan huynh <huynhxuanbui@gmail.com>
2020-05-17 13:30:51 +0700

    create class product

--- src/model/Product.java
+++ src/model/Product.java
@@ -1,4 +1,111 @@
 package model;
 
 public class Product {
+    private String productId;
+    private String categoryId;
+    private String productName;
+    private float productPrice;
+    private int quantityInStock;
+    private String image;
+    private int status;
+    private String description;
+    private String categoryName;
+
+    public Product() {
+    }
+
+    public Product(String productId, String categoryId, String productName, float productPrice, int quantityInStock, String image, int status, String description) {
+        this.productId = productId;
+        this.categoryId = categoryId;
+        this.productName = productName;
+        this.productPrice = productPrice;
+        this.quantityInStock = quantityInStock;
+        this.image = image;
+        this.status = status;
+        this.description = description;
+    }
+
+    public Product(String productId, String productName, float productPrice, int quantityInStock, String image, int status, String description, String categoryName) {
+        this.productId = productId;
+        this.productName = productName;
+        this.productPrice = productPrice;
+        this.quantityInStock = quantityInStock;
+        this.image = image;
+        this.status = status;
+        this.description = description;
+        this.categoryName = categoryName;
+    }
+
+    public String getProductId() {
+        return productId;
+    }
+
+    public void setProductId(String productId) {
+        this.productId = productId;
+    }
+
+    public String getCategoryId() {
+        return categoryId;
+    }
+
+    public void setCategoryId(String categoryId) {
+        this.categoryId = categoryId;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public float getProductPrice() {
+        return productPrice;
+    }
+
+    public void setProductPrice(float productPrice) {
+        this.productPrice = productPrice;
+    }
+
+    public int getQuantityInStock() {
+        return quantityInStock;
+    }
+
+    public void setQuantityInStock(int quantityInStock) {
+        this.quantityInStock = quantityInStock;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getCategoryName() {
+        return categoryName;
+    }
+
+    public void setCategoryName(String categoryName) {
+        this.categoryName = categoryName;
+    }
+
 }
